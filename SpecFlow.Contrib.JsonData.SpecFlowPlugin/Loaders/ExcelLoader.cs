8873d3d5b3baa01c585aa94457a55e8176fd936a
Kazantsev V.A <Kazantsevv.a@yandex.ru>
2021-10-02 12:47:24 +0300

    ExcelLoader refactoring
    ExcelLoader refactoring

--- SpecFlow.Contrib.JsonData.SpecFlowPlugin/Loaders/ExcelLoader.cs
+++ SpecFlow.Contrib.JsonData.SpecFlowPlugin/Loaders/ExcelLoader.cs
@@ -17,35 +17,8 @@ namespace SpecFlow.Contrib.JsonData.SpecFlowPlugin.Loaders
 
         protected override DataSource LoadDataSourceFromFilePath(string filePath, string dataSet)
         {
-            using FileStream stream = File.Open(filePath, FileMode.Open, FileAccess.Read);
-            using IExcelDataReader reader = ExcelReaderFactory.CreateReader(stream);
 
-            DataSet result = reader.AsDataSet(new ExcelDataSetConfiguration
-            {
-                ConfigureDataTable = (_) => new ExcelDataTableConfiguration()
-                {
-                    UseHeaderRow = true
-                }
-            });
-
-            System.Data.DataTable resultTable = default;
-            try
-            {
-                if (!result.Tables.Contains(dataSet))
-                {
-                    resultTable = result.Tables[0];
-                }
-                else
-                {
-                    resultTable = result.Tables[dataSet];
-                }
-                
-            }
-            catch (NullReferenceException)
-            {
-                throw new ExternalDataPluginException($"Unable to find worksheet {dataSet} in json file: {filePath}");
-            }
-
+            System.Data.DataTable resultTable = SelectDataTable(filePath, dataSet);
             DataTable dataTable = new DataTable(resultTable.Columns.OfType<DataColumn>().Select(c => c.ColumnName).ToArray());
 
             foreach (DataRow resultTableRow in resultTable.Rows)
@@ -59,6 +32,37 @@ namespace SpecFlow.Contrib.JsonData.SpecFlowPlugin.Loaders
             }
 
             return new DataSource(dataTable);
+        }
+
+        internal System.Data.DataTable SelectDataTable(string excelFilePath, string table)
+        {
+            using FileStream stream = File.Open(excelFilePath, FileMode.Open, FileAccess.Read);
+            using IExcelDataReader reader = ExcelReaderFactory.CreateReader(stream);
+
+            DataSet result = reader.AsDataSet(new ExcelDataSetConfiguration
+            {
+                ConfigureDataTable = (_) => new ExcelDataTableConfiguration()
+                {
+                    UseHeaderRow = true
+                }
+            });
+
+            try
+            {
+                if (!result.Tables.Contains(table))
+                {
+                    return result.Tables[0];
+                }
+                else
+                {
+                    return result.Tables[table];
+                }
+
+            }
+            catch (NullReferenceException)
+            {
+                throw new ExternalDataPluginException($"Unable to find worksheet {table} in json file: {excelFilePath}");
+            }
         }
     }
 }
