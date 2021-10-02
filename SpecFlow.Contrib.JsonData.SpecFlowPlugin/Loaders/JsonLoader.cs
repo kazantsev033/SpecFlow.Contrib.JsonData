8bbd692ba5df2e9c06c12ce76e927caf399e703a
Kazantsev V.A <Kazantsevv.a@yandex.ru>
2021-10-02 12:32:56 +0300

    JsonLoader refactoring
    - Remove var
    - Move selecting array to a separate method

--- SpecFlow.Contrib.JsonData.SpecFlowPlugin/Loaders/JsonLoader.cs
+++ SpecFlow.Contrib.JsonData.SpecFlowPlugin/Loaders/JsonLoader.cs
@@ -16,36 +16,40 @@ namespace SpecFlow.Contrib.JsonData.SpecFlowPlugin.Loaders
         protected override DataSource LoadDataSourceFromFilePath(string filePath, string dataSet)
         {
             JObject originalJson = JObject.Parse(ReadTextFileContent(filePath));
-            JArray jArray;
-            try
+            JArray jsonArray = SelectJsonArray(originalJson, dataSet);
+
+            List<string> header = jsonArray.First.ToObject<JObject>().Properties().Select(p => p.Name).ToList();
+
+            DataTable dataTable = new DataTable(header.ToArray());
+            foreach (JObject jsonObject in jsonArray.Select(i => i.ToObject<JObject>()))
             {
-                if (dataSet is null)
-                {
-                    jArray = originalJson.SelectToken("$.*").ToObject<JArray>();
-                }
-                else
-                {
-                    jArray = originalJson[dataSet].ToObject<JArray>();
-                }
-            }
-            catch (System.NullReferenceException)
-            {
-                throw new ExternalDataPluginException($"Unable to find array {dataSet} in json file: {filePath}");
-            }                    
-            
-            List<string> header = jArray.First.ToObject<JObject>().Properties().Select(p => p.Name).ToList();
-            
-            var dataTable = new DataTable(header.ToArray());
-            foreach (JObject childJson in jArray.Select(i => i.ToObject<JObject>()))
-            {
-                var dataRecord = new DataRecord();
-                foreach (JProperty property in childJson.Properties())
+                DataRecord dataRecord = new DataRecord();
+                foreach (JProperty property in jsonObject.Properties())
                 {
                     dataRecord.Fields[property.Name] = new DataValue(property.Value);
                 }
                 dataTable.Items.Add(dataRecord);
             }
             return new DataSource(dataTable);
+        }
+
+        internal JArray SelectJsonArray(JObject jsonObject, string jsonArrayName)
+        {
+            try
+            {
+                if (jsonArrayName is null)
+                {
+                    return jsonObject.SelectToken("$.*").ToObject<JArray>();
+                }
+                else
+                {
+                    return jsonObject[jsonArrayName].ToObject<JArray>();
+                }
+            }
+            catch (System.NullReferenceException)
+            {
+                throw new ExternalDataPluginException($"Unable to find array {jsonArrayName} in json file: {jsonObject}");
+            }
         }
     }
 }
